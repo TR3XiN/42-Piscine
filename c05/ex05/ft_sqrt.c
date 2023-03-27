1b14990064a82994ef60d0ffc7267ccd286420ed
Filipe Lima <frango@pop-os.localdomain>
2023-03-27 02:14:03 +0100

    42

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -1,41 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: frodrige <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/25 19:29:12 by frodrige          #+#    #+#             */
-/*   Updated: 2023/03/25 19:29:13 by frodrige         ###   ########.fr       */
+/*   Created: 2023/03/25 19:26:43 by frodrige          #+#    #+#             */
+/*   Updated: 2023/03/25 19:26:45 by frodrige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-void ft_str(char *str)
-{
-    int i;
-
-    i = 0;
-    while(str[i] != '\0')
-    {
-        ft_putchar(str[i]);
-        i++;
-    }
-}
-
-int main(int argc, char **argv)
-{
-    if(argc <= 1)
-    {
-        ft_str(argv[0]);
-        write(1, "\n", 1);
-    }
-    else    
-        write(1, "Erro 404", 9);
-}
